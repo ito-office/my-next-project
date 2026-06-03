a341828c7d18571d6e7a2d8afeda5c08565c3b7b
ito-office <ito.office.it@gmail.com>
2026-06-03 16:52:51 +0900

    20260603_第4章まで完了

---
+++ app/_components/Sheet/Sheet.tsx
@@ -0,0 +1,16 @@
+import styles from './Sheet.module.css'
+
+type Props = {
+    children: React.ReactNode;
+}
+
+export default function Sheet({ children }:Props) {
+    return(
+        <>
+            <div className={styles.container}>
+                {children}
+            </div>
+        </>
+    );
+    
+}
